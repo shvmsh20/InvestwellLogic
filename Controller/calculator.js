d9cd8b80102f67be99428743dd09dbcd30f04e4c
shvmsh20 <itsshivamsharma21@gmail.com>
2023-03-28 00:04:08 +0530

    worked on reviews

--- Controller/calculator.js
+++ Controller/calculator.js
@@ -1,56 +1,62 @@
-const service = require("../Services/calculator")
-const consts = require("../Constants/index")
+const service = require("../Services/calculator");
+const consts = require("../Constants/index");
 
+const calculate = async (req, res) => {
+    try {
+        const monthlyInvestment = parseInt(req.query.monthlyInvestment);
+        const investmentPeriod = parseInt(req.query.investmentPeriod);
+        const rateOfReturn = parseInt(req.query.rateOfReturn);
+        const delay = parseInt(req.query.delay);
 
-const calculate = async (req, res)=>{
+        if (
+            Number.isNaN(monthlyInvestment) ||
+            monthlyInvestment < consts.monthlyInvestmentMin ||
+            monthlyInvestment > consts.monthlyInvestmentMax
+        )
+            throw new Error("Invalid data entered");
 
-    try{
-
-        const isValid = (request) => {
-            
-            const monthlyInvestment = parseInt(request.monthlyInvestment);
-            const investmentPeriod = parseInt(request.investmentPeriod);
-            const rateOfReturn = parseInt(request.rateOfReturn);
-            const delay = parseInt(request.delay);
-
-            if(Number.isNaN(monthlyInvestment) || monthlyInvestment<consts.monthlyInvestmentMin || monthlyInvestment>consts.monthlyInvestmentMax)
-                throw new Error("Invalid data entered")
-            
-
-            if(Number.isNaN(investmentPeriod) || investmentPeriod<consts.investmentPeriodMin || investmentPeriod>consts.investmentPeriodMax){
-                throw new Error("Invalid data entered")
-            }
-
-            if(Number.isNaN(rateOfReturn) || rateOfReturn<consts.rateOfReturnMin || rateOfReturn>consts.rateOfReturnMax){
-                throw new Error("Invalid data entered")
-            }
-
-            if(Number.isNaN(delay) || delay<consts.delayMin || delay>consts.delayMax){
-                throw new Error("Invalid data entered")
-            }
+        if (
+            Number.isNaN(investmentPeriod) ||
+            investmentPeriod < consts.investmentPeriodMin ||
+            investmentPeriod > consts.investmentPeriodMax
+        ) 
+            throw new Error("Invalid data entered");
         
-            return true;
-        }
 
-    
-
-        isValid(req.query)
+        if (
+            Number.isNaN(rateOfReturn) ||
+            rateOfReturn < consts.rateOfReturnMin ||
+            rateOfReturn > consts.rateOfReturnMax
+        ) 
+            throw new Error("Invalid data entered");
         
+
+        if (
+            Number.isNaN(delay) ||
+            delay < consts.delayMin ||
+            delay > consts.delayMax
+        ) 
+            throw new Error("Invalid data entered");
+        
+
         const result = await service.calculate(req.query);
 
-        if(result instanceof Error){
-            res.send({status: -1, message: "Something is not good", result:result.message})
-        }else{
-            res.send({status: 0, message: "Result Successfull", result:result})
+        if (result instanceof Error) {
+            res.send({
+                status: -1,
+                message: "Something is not good",
+                result: result.message,
+            });
+        } else {
+            res.send({ status: 0, message: "Result Successfull", result: result });
         }
-        
-
-    }catch(error){
-
-        res.send({status: -1, message: "Something is not good", result:error.message})
-
+    } catch (error) {
+        res.send({
+            status: -1,
+            message: "Something is not good",
+            result: error.message,
+        });
     }
-    
-}
+};
 
-module.exports = {calculate};
+module.exports = { calculate };
--- Services/calculator.js
+++ Services/calculator.js
@@ -1,21 +1,21 @@
   function findAmount(monthlyInvestment, investmentPeriod, rateOfReturn, delay){
-    let Months = delay ? (investmentPeriod)*12 - delay : (investmentPeriod)*12;
-    let Rate = (rateOfReturn - 0)/12;
+    const months = delay ? (investmentPeriod)*12 - delay : (investmentPeriod)*12;
+    const rate = (rateOfReturn - 0)/12;
     let sipCumulation = 0;
     let sipGrowthResult = 0;
   
-    for(let i=1; i<=Months; i++)
+    for(let i=1; i<=months; i++)
     {
-      sipCumulation = monthlyInvestment*(Math.pow((1+Rate/100),i));
+      sipCumulation = monthlyInvestment*(Math.pow((1+rate/100),i));
       sipGrowthResult += sipCumulation;
     }
     return sipGrowthResult;
   }
-const calculate = async (obj)=>{
+const calculate = async ({monthlyInvestment, investmentPeriod, rateOfReturn, delay})=>{
 
   try{
-    const startToday = findAmount(obj.monthlyInvestment, obj.investmentPeriod, obj.rateOfReturn).toFixed(0);
-    const delayedStart = findAmount(obj.monthlyInvestment, obj.investmentPeriod, obj.rateOfReturn, obj.delay).toFixed(0);
+    const startToday = findAmount(monthlyInvestment, investmentPeriod, rateOfReturn).toFixed(0);
+    const delayedStart = findAmount(monthlyInvestment, investmentPeriod, rateOfReturn, delay).toFixed(0);
     const notionalLoss = (startToday - delayedStart);
     return {
         startToday,
