19bb81556bdffebd5045a3389564f0ff6d4102b0
shvmsh20 <itsshivamsharma21@gmail.com>
2023-03-30 17:34:19 +0530

    removed body parser

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ const route = require("./Routes/index.js");
 const PORT = process.env.PORT || 5000;
 
 
-//BodyParser
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
 app.use("/", route);
 
 
